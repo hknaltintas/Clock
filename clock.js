d08bb9857f116d33b066916cc237296b679acf83
Hakan Altintas <hhakanaltintas@gmail.com>
2023-12-10 15:16:23 +0300

    Clock project

---
+++ clock.js
@@ -0,0 +1,25 @@
+const secondHand=document.querySelector(".second-hand");
+const minuteHand=document.querySelector(".min-hand");
+const hourHand=document.querySelector(".hour-hand");
+
+
+function setDate()
+{
+    
+    const now=new Date();
+    const second=now.getSeconds();
+    const secondDegrees=(second/60)*360+90;
+    secondHand.style.transform=`rotate(${secondDegrees}deg)`
+
+    const minute=now.getMinutes();
+    const minuteDegrees=(minute/60)*360+90;
+    minuteHand.style.transform=`rotate(${minuteDegrees}deg)`
+
+    const hour=now.getHours();
+    const hourDegrees=(hour/60)*360+90;
+    hourHand.style.transform=`rotate(${hourDegrees}deg)`
+}
+    
+
+
+setInterval(setDate ,1000)
